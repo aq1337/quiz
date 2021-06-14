4d9ef749809781482dfb000dcfffdf967bad857f
aq1337 <kphil93@googlemail.com>
2021-06-14 09:57:48 +0200

    background added, components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NavbarComponent } from './navbar/navbar.component';
+import { StartpageComponent } from './startpage/startpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    StartpageComponent
   ],
   imports: [
     BrowserModule,
